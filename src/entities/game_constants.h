ee79e81b57c309bfb0e7467d7d60344a0e9cfaab
Mark Vajger <vajger.mark@gmail.com>
2026-03-28 22:41:20 +0100

    game_constants: adjust indentasions

--- src/entities/game_constants.h
+++ src/entities/game_constants.h
@@ -1,19 +1,23 @@
 #pragma once
 
+/*
+    Player stats
+*/
+
 // Warrior stats
-constexpr int   WARRIOR_HP      = 120;
-constexpr int   WARRIOR_ATTACK  = 12;
-constexpr int   WARRIOR_DEFENSE = 8;
-constexpr float WARRIOR_CRIT    = 0.0f;
+constexpr int   WARRIOR_HP              = 120;
+constexpr int   WARRIOR_ATTACK          = 12;
+constexpr int   WARRIOR_DEFENSE         = 8;
+constexpr float WARRIOR_CRIT            = 0.0f;
 
 // Mage stats
-constexpr int   MAGE_HP         = 70;
-constexpr int   MAGE_ATTACK     = 14;
-constexpr int   MAGE_DEFENSE    = 3;
-constexpr float MAGE_CRIT       = 0.0f;
+constexpr int   MAGE_HP                 = 70;
+constexpr int   MAGE_ATTACK             = 18;
+constexpr int   MAGE_DEFENSE            = 3;
+constexpr float MAGE_CRIT               = 0.0f;
 
 // Rogue stats
-constexpr int   ROGUE_HP        = 90;
-constexpr int   ROGUE_ATTACK    = 14;
-constexpr int   ROGUE_DEFENSE   = 5;
-constexpr float ROGUE_CRIT      = 0.3f;
+constexpr int   ROGUE_HP                = 90;
+constexpr int   ROGUE_ATTACK            = 14;
+constexpr int   ROGUE_DEFENSE           = 5;
+constexpr float ROGUE_CRIT              = 0.3f;
