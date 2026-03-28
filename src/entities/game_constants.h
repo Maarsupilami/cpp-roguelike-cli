73bc020d393e4f6a4984635600bab66bcb50fc92
Mark Vajger <vajger.mark@gmail.com>
2026-03-28 23:03:16 +0100

    fix: typo in chiefe and adjust indentation

--- src/entities/game_constants.h
+++ src/entities/game_constants.h
@@ -5,65 +5,65 @@
 */
 
 // Warrior stats
-constexpr int   WARRIOR_HP              = 120;
-constexpr int   WARRIOR_ATTACK          = 12;
-constexpr int   WARRIOR_DEFENSE         = 8;
-constexpr float WARRIOR_CRIT            = 0.0f;
+constexpr int       WARRIOR_HP                      = 120;
+constexpr int       WARRIOR_ATTACK                  = 12;
+constexpr int       WARRIOR_DEFENSE                 = 8;
+constexpr float     WARRIOR_CRIT                    = 0.0f;
 
 // Mage stats
-constexpr int   MAGE_HP                 = 70;
-constexpr int   MAGE_ATTACK             = 18;
-constexpr int   MAGE_DEFENSE            = 3;
-constexpr float MAGE_CRIT               = 0.0f;
+constexpr int       MAGE_HP                         = 70;
+constexpr int       MAGE_ATTACK                     = 18;
+constexpr int       MAGE_DEFENSE                    = 3;
+constexpr float     MAGE_CRIT                       = 0.0f;
 
 // Rogue stats
-constexpr int   ROGUE_HP                = 90;
-constexpr int   ROGUE_ATTACK            = 14;
-constexpr int   ROGUE_DEFENSE           = 5;
-constexpr float ROGUE_CRIT              = 0.3f;
+constexpr int       ROGUE_HP                        = 90;
+constexpr int       ROGUE_ATTACK                    = 14;
+constexpr int       ROGUE_DEFENSE                   = 5;
+constexpr float     ROGUE_CRIT                      = 0.3f;
 
 /*
     Enemy stats
 */
 // Goblin stats
-constexpr int   GOBLIN_HP               = 20;
-constexpr int   GOBLIN_ATTACK           = 8;
-constexpr int   GOBLIN_DEFENSE          = 2;
-constexpr int   GOBLIN_EXP_REWARD       = 30;
-constexpr int   GOBLIN_GOLD_REWARD      = 5;
+constexpr int       GOBLIN_HP                       = 20;
+constexpr int       GOBLIN_ATTACK                   = 8;
+constexpr int       GOBLIN_DEFENSE                  = 2;
+constexpr int       GOBLIN_EXP_REWARD               = 30;
+constexpr int       GOBLIN_GOLD_REWARD              = 5;
 
 // Skeleton stats
-constexpr int   SKELETON_HP             = 15;
-constexpr int   SKELETON_ATTACK         = 10;
-constexpr int   SKELETON_DEFENSE        = 4;
-constexpr int   SKELETON_EXP_REWARD     = 25;
-constexpr int   SKELETON_GOLD_REWARD    = 4;
+constexpr int       SKELETON_HP                     = 15;
+constexpr int       SKELETON_ATTACK                 = 10;
+constexpr int       SKELETON_DEFENSE                = 4;
+constexpr int       SKELETON_EXP_REWARD             = 25;
+constexpr int       SKELETON_GOLD_REWARD            = 4;
 
 // Troll stats
-constexpr int   TROLL_HP             = 40;
-constexpr int   TROLL_ATTACK         = 14;
-constexpr int   TROLL_DEFENSE        = 6;
-constexpr int   TROLL_EXP_REWARD     = 60;
-constexpr int   TROLL_GOLD_REWARD    = 10;
+constexpr int       TROLL_HP                        = 40;
+constexpr int       TROLL_ATTACK                    = 14;
+constexpr int       TROLL_DEFENSE                   = 6;
+constexpr int       TROLL_EXP_REWARD                = 60;
+constexpr int       TROLL_GOLD_REWARD               = 10;
 
 // Boss stats
-// Goblin Chiefe stats
-constexpr int   GOBLIN_CHIEF_HP             = 50;
-constexpr int   GOBLIN_CHIEF_ATTACK         = 12;
-constexpr int   GOBLIN_CHIEF_DEFENSE        = 5;
-constexpr int   GOBLIN_CHIEF_EXP_REWARD     = 80;
-constexpr int   GOBLIN_CHIEF_GOLD_REWARD    = 15;
+// Goblin Chief stats
+constexpr int       GOBLIN_CHIEF_HP                 = 50;
+constexpr int       GOBLIN_CHIEF_ATTACK             = 12;
+constexpr int       GOBLIN_CHIEF_DEFENSE            = 5;
+constexpr int       GOBLIN_CHIEF_EXP_REWARD         = 80;
+constexpr int       GOBLIN_CHIEF_GOLD_REWARD        = 15;
 
 // Stone Golem stats
-constexpr int   STONE_GOLEM_HP             = 80;
-constexpr int   STONE_GOLEM_ATTACK         = 18;
-constexpr int   STONE_GOLEM_DEFENSE        = 10;
-constexpr int   STONE_GOLEM_EXP_REWARD     = 150;
-constexpr int   STONE_GOLEM_GOLD_REWARD    = 25;
+constexpr int       STONE_GOLEM_HP                  = 80;
+constexpr int       STONE_GOLEM_ATTACK              = 18;
+constexpr int       STONE_GOLEM_DEFENSE             = 10;
+constexpr int       STONE_GOLEM_EXP_REWARD          = 150;
+constexpr int       STONE_GOLEM_GOLD_REWARD         = 25;
 
 // Dark Necromancer stats
-constexpr int   DARK_NECROMANCER_HP             = 120;
-constexpr int   DARK_NECROMANCER_ATTACK         = 22;
-constexpr int   DARK_NECROMANCER_DEFENSE        = 8;
-constexpr int   DARK_NECROMANCER_EXP_REWARD     = 300;
-constexpr int   DARK_NECROMANCER_GOLD_REWARD    = 50;
+constexpr int       DARK_NECROMANCER_HP             = 120;
+constexpr int       DARK_NECROMANCER_ATTACK         = 22;
+constexpr int       DARK_NECROMANCER_DEFENSE        = 8;
+constexpr int       DARK_NECROMANCER_EXP_REWARD     = 300;
+constexpr int       DARK_NECROMANCER_GOLD_REWARD    = 50;
